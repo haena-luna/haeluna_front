d28979f110d6b1e9790674dd25068748aaf30990
sungwon2054 <chsw93@gmail.com>
2021-05-19 17:10:22 +0900

    🔨 로그인 페이지 넘기기

--- src/component/router/NavBar.jsx
+++ src/component/router/NavBar.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-
-
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Login from '../user/Login';
 
 const NavBar = () => {
   return(
@@ -20,7 +19,7 @@ const NavBar = () => {
           <Typography variant="h6" style={style}>
             React User Application
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Link href="/login" color='#fff' >Login</Link>
         </Toolbar>
       </AppBar>
     </div>
